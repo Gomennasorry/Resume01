cf45e0dd709f04f2b943a1f94d58beb3ad63e9f3
Gomennasorry <79724773+Gomennasorry@users.noreply.github.com>
2024-02-18 14:50:51 +0700

    Initial commit

---
+++ Resume01/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Mvc;
+using Resume01.Models;
+using System.Diagnostics;
+
+namespace Resume01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Resume2()
+        {
+            return View();
+        }
+
+		public IActionResult Resume3()
+		{
+			return View();
+		}
+
+        public IActionResult Resume4()
+        {
+            return View();
+        }
+
+        public IActionResult Resume5()
+        {
+            return View();
+        }
+
+        public IActionResult TestLogic()
+        {
+            return View();
+        }
+
+		public IActionResult Resume6()
+		{
+			return View();
+		}
+
+        public IActionResult Resume6_5()
+        {
+            return View();
+        }
+
+        public IActionResult Resume7()
+        {
+            return View();
+        }
+        
+        public IActionResult Resume8()
+        {
+            return View();
+        }
+        
+        public IActionResult Shipment_Monitor()
+        {
+            return View();
+        }
+
+        public IActionResult Register_Info()
+        {
+            return View();
+        }
+
+        public IActionResult Testgap()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
