8cfde8d870f9975aef17a57e1c00458e52f353b9
Krirk Luangratchapan <krirk@generwiz.com>
2024-02-23 14:49:39 +0700

    krirk demo  Ver 1

--- Resume01/Controllers/HomeController.cs
+++ Resume01/Controllers/HomeController.cs
@@ -92,6 +92,10 @@ namespace Resume01.Controllers
         {
             return View();
         }
+        public IActionResult KrirkDemoDataTable()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Resume01/Program.cs
+++ Resume01/Program.cs
@@ -1,9 +1,99 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+
+string systemID = string.Empty;
+string bearerToken = string.Empty;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//systemID = builder.Configuration["SystemID"];
+
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddControllersWithViews().AddJsonOptions(options =>
+{
+    // Use the default property (Pascal) casing.
+    
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromDays(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+////Provide a secret key to Encrypt and Decrypt the Token
+//var SecretKey = Encoding.ASCII.GetBytes($"{builder.Configuration["JwtToken:SigningKey"]}");
+////Configure JWT Token Authentication
+//builder.Services.AddAuthentication(auth =>
+//{
+//    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//    auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(token =>
+//{
+//    token.RequireHttpsMetadata = false;
+//    token.SaveToken = true;
+//    token.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidateIssuerSigningKey = true,
+//        RequireExpirationTime = true,
+//        ValidIssuer = builder.Configuration["JwtToken:Issuer"],
+//        ValidAudience = builder.Configuration["JwtToken:Audience"],
+//        IssuerSigningKey = new SymmetricSecurityKey(SecretKey),
+//        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+//    };
+//});
+
+//builder.Services.AddHttpClient("BaseClient", client =>
+//{
+//    client.BaseAddress = new Uri(builder.Configuration[$"BASEApi_{systemID}"]);
+
+//    // Add authorization if found
+//    if (!string.IsNullOrEmpty(bearerToken))
+//    {
+//        client.DefaultRequestHeaders.Accept.Clear();
+//        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+//        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+//    }
+
+//});
+
+builder.Services.AddHttpContextAccessor();
+
+
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    if (context.Response.StatusCode == (int)HttpStatusCode.Unauthorized) // 401
+    {
+        //context.Response.ContentType = "application/json";
+        //await context.Response.WriteAsync("StatusCode = 401, Message = Token is not valid");
+
+        context.Response.Redirect("Home/BasicForm");
+        return;
+    }
+
+    await next(context);
+});
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,12 +106,43 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+app.UseSession();
+app.Use(async (context, next) =>
+{
+    //Add JWToken to all incoming HTTP Request Header
+    //Must set on above JWToken Authentication service
+    //bearerToken = context.Session.GetString(SESSIONID.BEAR_TOKE);
+    bearerToken = context.Session.GetString("BEAR_TOKE");
+    context.Request.Headers.Add("Authorization", "Bearer " + bearerToken);
+
+    await next.Invoke();
+});
+
+
+
 app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseStatusCodePages(context => {
+    var response = context.HttpContext.Response;
+    if (response.StatusCode == (int)HttpStatusCode.Unauthorized ||
+        response.StatusCode == (int)HttpStatusCode.Forbidden)
+        response.Redirect("/Authen/LoginPage");
+    return Task.CompletedTask;
+});
+
+app.UseAuthorization();
+
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+//App.SystemID = systemID.Equals("PRD") ? "PRD" : "DEV";
+
 
 app.Run();
